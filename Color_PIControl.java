b536e9de90e27c25b91844827f6ffe0706e9b91c
Brytni Richards <35405719+BrytniR@users.noreply.github.com>
2018-11-17 18:58:56 -0500

    Fixed Spacing

--- Color_PIControl.java
+++ Color_PIControl.java
@@ -10,55 +10,51 @@ import lejos.hardware.lcd.*;
 public class Color_PIControl {
 
 	public static void main(String[] args) throws Exception {
+	EV3ColorSensor color = new EV3ColorSensor(SensorPort.S4);
+	int sampleSize = color.sampleSize(); //size of port
+	float[] actual = new float[sampleSize];
 
-    EV3ColorSensor color = new EV3ColorSensor(SensorPort.S4);
+	double desired = 0.2;
+	double kp = 100; //proportional feedback constant
+	double ki = 0.1; //integral feedback constant
+	double integral = 0;
+	double motorBspd = 50;
+	double motorCspd = 50;
+	double error, correction;
+	Motor.B.setSpeed((int)motorBspd);//initialize motor speed
+	Motor.C.setSpeed((int)motorCspd);
+	Motor.B.forward();
+	Motor.C.forward();
 
-    int sampleSize = color.sampleSize(); //size of port
+	while(!Button.ENTER.isDown()) {//Exit condition: press center button
+		color.getRedMode().fetchSample(actual, 0); //get color sensor value
+		error = desired - actual[0]; //difference from expected value
+		correction = kp * error + integral * ki;
 
-    float[] actual = new float[sampleSize];
+		//set speeds based on feedback controller and which direction to go
+		if (error > 0.05 && motorBspd < 200) {
+			motorBspd = 50 + correction;
+			motorCspd = 20 + correction;
+			integral += error;
+		}
 
-		double desired = 0.2;
-		double kp = 100; //proportional feedback constant
-		double ki = 0.1; //integral feedback constant
-		double integral = 0;
-    double motorBspd = 50;
-    double motorCspd = 50;
-    double error, correction;
-    Motor.B.setSpeed((int)motorBspd);//initialize motor speed
+		else if (error < -0.05 && motorCspd < 200) {
+			motorCspd = 50 + correction;
+			motorBspd = 20 + correction;
+			integral += error;
+		}
+		else {
+			motorCspd = 50;
+			motorBspd = 50;
+			integral += error;
+		}
+
+		Motor.B.setSpeed((int)motorBspd);
+		Motor.B.forward();
 		Motor.C.setSpeed((int)motorCspd);
-
-    Motor.B.forward();
-    Motor.C.forward();
-
-		while(!Button.ENTER.isDown()) {//Exit condition: press center button
-      color.getRedMode().fetchSample(actual, 0); //get color sensor value
-      error = desired - actual[0]; //difference from expected value
-      correction = kp * error + integral * ki;
-
-			//set speeds based on feedback controller and which direction to go
-      if (error > 0.05 && motorBspd < 200) {
-          motorBspd = 50 + correction;
-          motorCspd = 20 + correction;
-          integral += error;
-        }
-
-        else if (error < -0.05 && motorCspd < 200) {
-          motorCspd = 50 + correction;
-          motorBspd = 20 + correction;
-          integral += error;
-        }
-        else {
-      	  motorCspd = 50;
-      	  motorBspd = 50;
-      	  integral += error;
-        }
-
-    	  Motor.B.setSpeed((int)motorBspd);
-    	  Motor.B.forward();
-    	  Motor.C.setSpeed((int)motorCspd);
-    	  Motor.C.forward();
-				LCD.clear();
-				System.out.println(correction);
+		Motor.C.forward();
+		LCD.clear();
+		System.out.println(correction);
 
 		}
 	color.close();
